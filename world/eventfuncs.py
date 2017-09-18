a6c1e7ab2017842bdf5ca0207d282fd73ee45e40
Vincent Le Goff <vincent.legoff.srs@gmail.com>
2017-09-18 10:41:36 +0200

    Add a simple eventfunc to get the game time

---
+++ world/eventfuncs.py
@@ -0,0 +1,35 @@
+"""
+Module containing the Avenew event funcs (event functions).
+"""
+
+import datetime
+
+from evennia.utils import gametime as gtime
+
+class GTime(datetime.datetime):
+
+    """Extended datetime, with a 'season' property."""
+
+    @property
+    def season(self):
+        """Return the game time season."""
+        Y = self.year
+        date = datetime.date
+        seasons = [('winter', (date(Y,  1,  1),  date(Y,  3, 20))),
+               ('spring', (date(Y,  3, 21),  date(Y,  6, 20))),
+               ('summer', (date(Y,  6, 21),  date(Y,  9, 22))),
+               ('autumn', (date(Y,  9, 23),  date(Y, 12, 20))),
+               ('winter', (date(Y, 12, 21),  date(Y, 12, 31)))
+        ]
+
+        today = self.date()
+        return next(season for season, (start, end) in seasons if start <= today <= end)
+
+
+def gametime():
+    """Return the current game time as a datetime object.
+
+    This returns a datetime opbject with an additional property: `season`.  It returns the current reason as a string (like "winter").
+
+    """
+    return GTime.fromtimestamp(gtime.gametime(absolute=True))
