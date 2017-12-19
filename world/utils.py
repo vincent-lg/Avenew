c2c40febbf639818aa7038b53be9cf31c9dc6f12
Vincent Le Goff <vincent.legoff.srs@gmail.com>
2017-12-19 09:13:26 +0100

    Add escape characters to latinify

--- world/utils.py
+++ world/utils.py
@@ -9,7 +9,36 @@ Functions:
 """
 
 _UNICODE_MAPPING = {
-    # Insert characters to escape here
+    u"\u00bc": "OE",
+    u"\u00bd": "oe",
+    u"\u00c0": u"A",
+    u"\u00c2": u"A",
+    u"\u00c7": u"C",
+    u"\u00c8": u"E",
+    u"\u00c9": u"E",
+    u"\u00ca": u"E",
+    u"\u00cb": u"E",
+    u"\u00ce": u"I",
+    u"\u00cf": u"I",
+    u"\u00d4": u"O",
+    u"\u00d9": u"U",
+    u"\u00db": u"U",
+    u"\u00dc": u"U",
+    u"\u00e0": u"a",
+    u"\u00e2": u"a",
+    u"\u00e7": u"c",
+    u"\u00e8": u"e",
+    u"\u00e9": u"e",
+    u"\u00ea": u"e",
+    u"\u00eb": u"e",
+    u"\u00ee": u"i",
+    u"\u00ef": u"i",
+    u"\u00f4": u"o",
+    u"\u00f9": u"u",
+    u"\u00fb": u"u",
+    u"\u00fc": u"u",
+    u"\u0152": u"OE",
+    u"\u0153": u"oe",
 }
 
 def latinify(unicode_string, replace=u"?", mapping=_UNICODE_MAPPING):
