f8437b28a9e6fa556db6c69572dde08c89a5d8dd
Vincent Le Goff <vincent.legoff.srs@gmail.com>
2018-01-30 08:34:31 +0100

    Place `get_numbered_name` in the shared typeclass

--- typeclasses/shared.py
+++ typeclasses/shared.py
@@ -33,6 +33,35 @@ class AvenewObject(object):
             return "{}(#{})".format(self.name, self.id)
         return self.name
 
+    def get_numbered_name(self, count, looker, **kwargs):
+        """
+        Return the numbered (singular, plural) forms of this object's key. This is by default called
+        by return_appearance and is used for grouping multiple same-named of this object. Note that
+        this will be called on *every* member of a group even though the plural name will be only
+        shown once. Also the singular display version, such as 'an apple', 'a tree' is determined
+        from this method.
+
+        Args:
+            count (int): Number of objects of this type
+            looker (Object): Onlooker. Not used by default.
+        Kwargs:
+            key (str): Optional key to pluralize, use this instead of the object's key.
+        Returns:
+            singular (str): The singular form to display.
+            plural (str): The determined plural form of the key, including the count.
+        """
+        key = kwargs.get("key", self.key)
+        singular = key
+        plural = "{} {}".format(count, self.attributes.get("plural", "things"))
+        if not self.aliases.get(plural, category="plural_key"):
+            # we need to wipe any old plurals/an/a in case key changed in the interrim
+            self.aliases.clear(category="plural_key")
+            self.aliases.add(plural, category="plural_key")
+            # save the singular form as an alias here too so we can display "an egg" and also
+            # look at 'an egg'.
+            self.aliases.add(singular, category="plural_key")
+        return singular, plural
+
     def search(self, searchdata, **kwargs):
         """
         Returns an Object matching a search string/condition
