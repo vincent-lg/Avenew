66ed3768ccd2ecf4dd8e10e38684ac2634d3b9d0
Vincent Le Goff <vincent.legoff.srs@gmail.com>
2018-03-17 16:06:56 +0100

    Add a simplistic `show_list` function

--- world/utils.py
+++ world/utils.py
@@ -5,6 +5,7 @@ Utility functions, specific to Avenew but generic enough to be ported to other p
 
 Functions:
     latinify(unicode[, default][, mapping]): return a unicode string containing only ASCII.
+    show_list(strings, width=4, **kwargs): return a formatted list.
 
 """
 
@@ -35,3 +36,50 @@ def latinify(unicode_string, replace=u"?", mapping=_UNICODE_MAPPING):
             unicode_string = unicode_string[:i] + replace + unicode_string[i + 1:]
 
     return unicode_string
+
+def show_list(strings, width=4, vertical=False, length=None, begin="",
+        between_lines="\n"):
+    """Show a formatted list in a ls-like display.
+
+    Args:
+        strings (list of str): the list of strings to display.
+        width (int): the number of columns per line.
+        vertical (bool): should the strings be added vertically?
+        length (int): the length of each column.
+        begin (str): the beginning of each line in the table.
+        between_lines (str): what to put between lines?
+
+    This function takes a list of strings as argument, and format it
+    in a table with a fixed number per line.  Other options are used to
+    give more freedom regarding formatting.
+
+    """
+    lines = [[]]
+    i = 0
+    max_length = 0
+
+    # Add the strings horizontally or vertically
+    for entry in strings:
+        line = lines[-1]
+        if len(line) >= width:
+            line = []
+            lines.append(line)
+        line.append(entry)
+        if len(entry) > max_length:
+            max_length = len(entry)
+
+    # Create the string
+    ret = ""
+    if length is None:
+        length = max_length
+
+    for i, line in enumerate(lines):
+        if i != 0:
+            ret += between_lines
+        ret += begin
+        for entry in line:
+            if len(entry) > length - 1:
+                entry = entry[:length - 4] + "..."
+            ret += entry.ljust(length)
+
+    return ret
