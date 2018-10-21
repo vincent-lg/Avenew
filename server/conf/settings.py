974393dfb6eb5eed7dba1d3acb8dd339de07a0f4
Vincent Le Goff <vincent.legoff.srs@gmail.com>
2018-10-21 19:54:40 +0200

    Add the background task to send emails (require django-background-tasks)

--- server/conf/settings.py
+++ server/conf/settings.py
@@ -65,6 +65,7 @@ SCREENREADER_REGEX_STRIP = r"\+-+|\+$|\+~|---+|~~+|==+"
 
 # Web
 INSTALLED_APPS += (
+        "background_task",
         "evennia_wiki",
         "web.evapp",
         "web.builder",
@@ -77,6 +78,35 @@ WEBSOCKET_CLIENT_ENABLED = False
 
 ## Communication
 TEST_SESSION = False
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'simple': {
+            'format': '%(asctime)s [%(levelname)s] %(message)s'
+        },
+    },
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'level': 'INFO',
+        },
+        'tasks_file': {
+            'level': 'INFO',
+            'class': 'logging.FileHandler',
+            'filename': 'server/logs/tasks.log',
+            'formatter': 'simple',
+        },
+    },
+    'loggers': {
+        'background_task': {
+            'handlers': ['tasks_file', 'console'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}
 
 try:
     from server.conf.secret_settings import *
