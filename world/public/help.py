0e2870678dbb4bf94c7dab2fc460285603bb8ee1
Vincent Le Goff <vincent.legoff.srs@gmail.com>
2018-10-29 16:59:58 +0100

    Translate the driving help file

--- world/public/help.py
+++ world/public/help.py
@@ -5,67 +5,70 @@
 from world.batch import get_help_entry
 
 #CODE
-get_help_entry("vehicle", """
-    In Avenew One, you can drive a vehicle using a set of commands discussed here.
-    Although vehicles can access areas you can not walk to, they're primarily used
-    to earn some time.  Cars, for instance, offer a convenient shortcut from
-    across town, although you could walk.  Typical vehicles would be cars,
-    trucks, motorcycles.  More atypical vehicles are still to be expected
-    however.
+get_help_entry("véhicle", """
+    Dans Avenew One, vous pouvez conduire un véhicule en utilisant un ensemble de commandes
+    décrites dans cette page d'aide. Bien que les véhicules puissent accéder à des zones
+    inaccessibles, ils sont principalement utilisés pour gagner du temps. Les voitures,
+    par exemple, offrent un raccourci pratique pour se déplacer dans la ville, bien que
+    vous puissiez marcher. Les véhicules typiques seraient les voitures, camions, motos.
+    Des véhicules plus atypiques sont encore à prévoir toutefois.
 
-    To drive a vehicle you own, or a public vehicle you have access to, you first
-    need to be near it.  You will see it in the room description.  Vehicles are
-    grouped by type and brand so as not to overflow you with data, but you can
-    easily search for a specific vehicle.  You have to climb into it, using the
-    |yenter|n command.  This command takes a single argument: the name (or a
-    portion of the name) of the
-    vehicle in which you want to climb.  You can also supply the number plate,
-    to avoid confusion.  Note that the vehicle might be locked, in which case,
-    you will need to use the |yunlock|n command.  You will not be able to
-    unlock every vehicle in a parking lot, you will need the keys or a similar
-    item.  Public vehicles are usually unlocked, which means you can enter into
-    them freely.  On the other hand, it also means anyone can do the same, so
-    you shouldn't rely too much on this mean of transportation.
+    Pour conduire un véhicule que vous possédez ou un véhicule public auquel vous avez
+    accès, vous devez d’abord vous trouvez auprès du dit véhicule. Vous le verrez dans
+    la description de la salle. Les véhicules sont regroupés par type et par marque afin de
+    ne pas vous surcharger d'information, mais vous pouvez rechercher facilement un véhicule
+    spécifique. Vous devez y monter en utilisant la commande |yenter|n. Vous devez
+    préciser en paramètre le nom (ou une partie du nom) du véhicule dans lequel vous
+    voulez monter. Vous pouvez également fournir la plaque d'immatriculation, pour
+    éviter la confusion. Notez que le véhicule peut être verrouillé, auquel cas,
+    vous devrez utiliser la commande |yunlock|n. Vous ne pourrez pas déverrouillez tous
+    les véhicules dans un parking, vous aurez besoin des clés ou d’un autre moyen de
+    déverrouiller ce véhicule. Les véhicules publics sont généralement
+    déverrouillés, ce qui signifie que vous pouvez entrer dedans sans contrainte. D'autre
+    part, cela signifie également que tout le monde peut faire la même chose,
+    peut-être ne devriez-vous pas trop vous fier à ce moyen de transport.
 
-    Once in the front seat of the vehicle, you have to use the |ydrive|n command
-    to grip the steering wheel.  This will not be possible if someone else is
-    already driving.  You will use the same command to release the steering wheel
-    when you want to stop driving.  If you are parked and you use the |ydrive|n
-    command, you will start the engine and drive the few yards necessary to
-    be on the street.
+    Une fois sur le siège avant du véhicule, vous devez utiliser la commande |ydrive|n
+    pour saisir le volant. Il n'est pas possible de conduire si quelqu'un d'autre est
+    déjà derrière le volant. Vous utiliserez la même commande pour relâcher le volant
+    quand vous voulez arrêter de conduire. Si vous êtes garé et que vous utilisez la
+    commande |ydrive|n, vous démarrerez le moteur et avancerez les quelques mètres nécessaires pour
+    être dans la rue.
 
-    You now have to accelerate.  You can control the speed using the |yspeed|n
-    command.  Specify your desired speed in miles per hour.  Notice that this
-    is more like your maximum cruising speed.  You will not have to manually
-    brake at every obstacle.  You will just try to drive as close from your
-    cruising speed as possible.  If you want to stop, simply use |yspeed 0|n
-    to slowly reduce speed.
+    Vous devez maintenant accélérer. Vous pouvez contrôler la vitesse en utilisant
+    la commande |yspeed|n. Spécifiez votre vitesse désirée en kilomètres par heure.
+    Notez que cela ressemble plus à votre vitesse de croisière maximale. Il ne sera
+    pas nécessaire de freiner manuellement à chaque obstacle. Vous allez juste
+    essayer de conduire au plus près possible de cette vitesse de croisière.
+    Si vous voulez arrêter complètement le véhicule, utilisez simplement |yspeed 0|n
+    qui réduira lentement votre vitesse.
 
-    A little before you arrive at a crossroad at the end of the street, you will
-    get notified about what exits are available in this crossroad.  This is
-    much like obtaining the obvious exits when you look at a room.  However,
-    you must choose a turn relatively quickly.  If you don't and the vehicle
-    enters the crossroad, you will either go forward if possible, or stop in
-    the middle of the crossroad.  In the latter case, you will need to turn and
-    then change your speed again with the |yspeed|n command.  To prepare to
-    turn when you get notified about the available exits, you can use the
-    |yturn|n command.  This, however, is not the easiest solution: you can
-    use direction names to turn, like |ynortheast|n or even the alias |yne|n.
-    This is quite easier to type and quicker, not mentioning that since
-    standard exits have the same name, you can use your client aliases or
-    macros if available.
+    Un peu avant d'arriver à un carrefour au bout de la rue, vous serez informé des
+    sorties disponibles dans ce carrefour. C'est un peu comme obtenir les sorties
+    visibles quand vous regardez une salle. Cependant, vous devez choisir un tournant
+    relativement rapidement. Si vous ne choisissez pas la direction à suivre, et que
+    le véhicule entre dans le carrefour, vous prendrez automatiquement la sortie
+    directement en face de vous si possible. Si cette sortie n'existe pas, le
+    véhicule s'arrêtera au milieu du carrefour. Dans ce dernier cas, vous devrez
+    tourner et changez à nouveau votre vitesse avec la commande |yspeed|n.
+    Une fois averti des sorties disponibles dans un carrefour, vous pouvez utiliser
+    la commande |yturn|n. Ce n’est cependant pas la solution la plus simple : vous
+    pouvez utiliser les noms de direction pour tourner, comme |ynord-est|n ou même
+    l'alias |yne|n. Cela est bien plus rapide à écrire, et puisque les sorties
+    standard utilisent les mêmes noms, vous pouvez aller encore plus vite avec les
+    macros de votre client, si il les supporte.
 
-    Once you arrive at your desired destination, first slow down using the
-    |yspeed|n command.  To park, you can use the |ypark|n command.  Your
-    vehicle should be relatively slow or you won't be able to use this command.
-    The |ypark|n command will attempt to park your vehicle on the right side of
-    the street.  If you prefer to park on the left, use the absolute direction
-    name or alias, like |ypark south|n or |ypark e|n.
+    Une fois arrivé à la destination souhaitée, commencez par ralentir à l’aide de la
+    commande |yspeed|n. Pour vous garer, vous pouvez utiliser la commande |ypark|n.
+    Votre véhicule doit être relativement lent ou vous ne pourrez pas utiliser cette
+    commande. La commande |ypark|n tentera de garer votre véhicule du côté droit de
+    la rue. Si vous préférez vous garer sur la gauche, utilisez la direction absolue
+    (son nom ou alias), comme |ypark sud|n ou |ypark e|n.
 
-    Finally, you can just climb out of your vehicle using the |yleave|n command.
-    Once more, you might have to unlock the vehicle as some will automatically
-    lock when you are driving to avoid possible thefts.  Once outside, you
-    might want to |ylock|n your vehicle to prevent others from climbing
-    into them and driving away.
+    Enfin, vous pouvez simplement sortir de votre véhicule à l’aide de la commande
+    |yleave|n. Une fois de plus, vous devrez peut-être déverrouiller le véhicule car
+    certains ne le feront pas automatiquement pour des raisons de sécurité. Une
+    fois à l'extérieur, vous pourriez vouloir utiliser la commande |ylock|n afin de
+    verrouiler votre véhicule et empêcher les autres de vous les voler.
 
-    """, aliases=["driving", "driver"])
+    """, aliases=["conducteur", "voiture", "camion", "moto"])
